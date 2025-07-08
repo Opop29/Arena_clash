fce5ffd96f73268eb5a4ab1014f5fd173b306603
Opop29 <joshuaquidit123456@gmail.com>
2025-07-08 23:09:06 +0800

    Updates

--- assets/ios.transition-legacy-60NTK8iP.js
+++ assets/ios.transition-legacy-60NTK8iP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CoDgrWvB.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-Bj7hnyBj.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
